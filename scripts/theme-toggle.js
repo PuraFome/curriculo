27e9268d360f9af5340046e1dc3b230e4673593a
Pura Fome <samuelpe06062000@gmail.com>
2024-11-27 19:10:28 -0300

    fix: alter color of dark mode and utfpr icon

--- scripts/theme-toggle.js
+++ scripts/theme-toggle.js
@@ -1,10 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
     const themeToggle = document.getElementById('theme-toggle');
     const body = document.body;
+    const universityImage = document.getElementById('university-img');
 
     const applyTheme = (theme) => {
         body.classList.toggle('dark-mode', theme === 'dark-mode');
         themeToggle.textContent = theme === 'dark-mode' ? '☀️' : '🌙';
+        if (universityImage) {
+            universityImage.src = theme === 'dark-mode' ? 'icons/utfpr-dark.png' : 'icons/utfpr.png';
+        }
     };
 
     const savedTheme = localStorage.getItem('theme');
